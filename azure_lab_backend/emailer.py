61f2eb033e8e9b3841264ff215a89fd948f03e9b
the1bit <tibor.kiss@cloudmentor.hu>
2025-03-25 22:24:43 +0100

    Portal links

--- azure_lab_backend/emailer.py
+++ azure_lab_backend/emailer.py
@@ -7,6 +7,8 @@ import os
 def send_lab_ready_email(username: str, password: str, recipient: str):
     configuration = sib_api_v3_sdk.Configuration()
     configuration.api_key["api-key"] = os.getenv("BREVO_API_KEY")
+    azure_portal_url = os.getenv("PORTAL_AZURE_URL")
+    aws_portal_url = os.getenv("PORTAL_AWS_URL")
 
     api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
     subject = "A labor környezeted elkészült!"
@@ -14,9 +16,15 @@ def send_lab_ready_email(username: str, password: str, recipient: str):
     to = [{"email": recipient}]
     html_content = f"""
     <p>Szia <b>{username}</b>!</p>
-    <p>A lab környezeted elkészült. Jelentkezz be az <a href='https://portal.azure.com'>Azure Portalba</a>.</p>
+    <p>A lab környezeted elkészült. Jelentkezz be az <a href='https://portal.azure.com'>Azure Portal-ba</a>.</p>
     <p><b>Felhasználónév:</b> {username}<br><b>Jelszó:</b> {password}</p>
-    <p>A lab 1 óra múlva automatikusan törlődik.</p>
+    <p>A lab 60 perc múlva automatikusan törlődik.</p>
+    
+    <p>Egyég linkek:</p>
+    <ul>
+        <li><a href='{azure_portal_url}' target='_blank'>Azure Portal</a></li>
+        <li><a href='{aws_portal_url}' target='_blank'>AWS Console</a></li>
+    
     <p>Üdv, <br> CloudMentor</p>
     """
 
