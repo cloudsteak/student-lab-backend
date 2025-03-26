c2676b40ce1c660b8455ab848b42ffa508100f53
the1bit <tibor.kiss@cloudmentor.hu>
2025-03-26 01:20:26 +0100

    Email

--- lab-backend/emailer.py
+++ lab-backend/emailer.py
@@ -4,11 +4,19 @@ import sib_api_v3_sdk
 from sib_api_v3_sdk.rest import ApiException
 import os
 
-def send_lab_ready_email(username: str, password: str, recipient: str):
+def send_lab_ready_email(username: str, password: str, recipient: str, cloud_provider: str):
     configuration = sib_api_v3_sdk.Configuration()
     configuration.api_key["api-key"] = os.getenv("BREVO_API_KEY")
     azure_portal_url = os.getenv("PORTAL_AZURE_URL")
     aws_portal_url = os.getenv("PORTAL_AWS_URL")
+    cloud_console_url = ""
+    if cloud_provider == "azure":
+        cloud_console_url = azure_portal_url
+    elif cloud_provider == "aws":
+        cloud_console_url = aws_portal_url
+    
+    print(cloud_console_url)
+        
 
     api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
     subject = "A labor környezeted elkészült!"
@@ -16,14 +24,10 @@ def send_lab_ready_email(username: str, password: str, recipient: str):
     to = [{"email": recipient}]
     html_content = f"""
     <p>Szia <b>{username}</b>!</p>
-    <p>A lab környezeted elkészült. Jelentkezz be az <a href='https://portal.azure.com'>Azure Portal-ba</a>.</p>
+    <p>A lab környezeted elkészült. <a href='{cloud_console_url}' target='_blank'>Webes bejelentkezés ({cloud_provider}) itt</a>.</p>
     <p><b>Felhasználónév:</b> {username}<br><b>Jelszó:</b> {password}</p>
     <p>A lab 60 perc múlva automatikusan törlődik.</p>
     
-    <p>Egyég linkek:</p>
-    <ul>
-        <li><a href='{azure_portal_url}' target='_blank'>Azure Portal</a></li>
-        <li><a href='{aws_portal_url}' target='_blank'>AWS Console</a></li>
     
     <p>Üdv, <br> CloudMentor</p>
     """
