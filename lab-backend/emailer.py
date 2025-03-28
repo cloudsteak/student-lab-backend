dcd7d479c3147775dd11d63f0deda82ecb401fbb
the1bit <tibor.kiss@cloudmentor.hu>
2025-03-29 00:02:30 +0100

    Lab email

--- lab-backend/emailer.py
+++ lab-backend/emailer.py
@@ -9,16 +9,11 @@ def send_lab_ready_email(username: str, password: str, recipient: str, cloud_pro
     configuration.api_key["api-key"] = os.getenv("BREVO_API_KEY")
     azure_portal_url = os.getenv("PORTAL_AZURE_URL")
     aws_portal_url = os.getenv("PORTAL_AWS_URL")
-    print("Linkek az e-mailhez:")
-    print(azure_portal_url)
-    print(aws_portal_url)
     cloud_console_url = ""
     if cloud_provider == "azure":
         cloud_console_url = azure_portal_url
     elif cloud_provider == "aws":
         cloud_console_url = aws_portal_url
-    
-    print(cloud_console_url)
         
 
     api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
@@ -27,17 +22,12 @@ def send_lab_ready_email(username: str, password: str, recipient: str, cloud_pro
     to = [{"email": recipient}]
     html_content = f"""
     <p>Szia <b>{username}</b>!</p>
-    <p>A lab környezeted elkészült. <a href='{cloud_console_url}' target='_blank'>Webes bejelentkezés ({cloud_provider}) itt</a>.</p>
-    {cloud_console_url}
-    Linkek:
-    {os.getenv("PORTAL_AZURE_URL")}
-    {os.getenv("PORTAL_AWS_URL")}
-    {os.getenv("LAB_TTL_SECONDS")}
+    <p>A lab környezeted elkészült. Kattinnts <a href='{cloud_console_url}' target='_blank'>ide a webes bejelentkezéshez ({upper(cloud_provider)})</a>.</p>
     <p><b>Felhasználónév:</b> {username}<br><b>Jelszó:</b> {password}</p>
-    <p>A lab 60 perc múlva automatikusan törlődik.</p>
+    <p><strong>A lab {int(os.getenv("LAB_TTL_SECONDS"))/60} perc múlva automatikusan törlődik.</strong></p>
     
     
-    <p>Üdv, <br> CloudMentor</p>
+    <p>Üdv, <br>Kiss Tibor - CloudMentor</p>
     """
 
     email = sib_api_v3_sdk.SendSmtpEmail(to=to, html_content=html_content, sender=sender, subject=subject)
